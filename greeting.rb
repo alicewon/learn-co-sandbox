f84c59b543359190b9d7c8bc1f8d8e991c69eaec
Alice Won <alicewon4@gmail.com>
2020-07-26 23:20:57 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,25 @@
+=begin
+def greeting
+  puts "Hello World"
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
+
+
+
+def greeting_programmer(name, language)
+  puts "Hello #{name}. We heard you are a great #{language} programmer"
+end
+
+greeting_programmer("Maria", "ruby")
+=end
+
+
+new_hash = {
+  :created => Time.now,
+  :message => "Hello World!"
+}
